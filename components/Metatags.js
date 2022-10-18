cf71491eda87efd2b6be5d88a75df08e267f588c
mitch <mitchell.n@outlook.com>
2022-10-18 14:03:09 +0200

    Blog

---
+++ components/Metatags.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Metatags({
+  title = "Techgrounds",
+  description = "Testers website gebouwd in Next js en Firebase",
+  image = "https://manuarora.in/logo.png",
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@fireship_dev" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+    </Head>
+  );
+}
